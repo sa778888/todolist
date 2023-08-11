6a50f3c40e48cda0f2b750ec4c6705249b798ca1
sa778888 <arshlaansiddiquie@gmail.com>
2023-08-12 04:37:13 +0530

    first commit

---
+++ todolist/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TodoWrapper } from "./components/TodoWrapper";
+
+function App() {
+  return (
+    <div>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
